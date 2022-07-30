4419bbec598e80f80681f6c185cccfaa6efdf3cc
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-07-29 23:26:07 -0500

    back button function

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,7 +4,9 @@
         <div class="col-2">
             <div class="border rounded-circle d-flex justify-content-center align-items-center"
                 style="height: 40px; width: 40px;">
-                <i class="fas fa-angle-left"></i>
+                <a href="{{ route('home') }}">
+                    <i class="fas fa-angle-left"></i>
+                </a>
             </div>
         </div>
         {{-- search bar --}}
