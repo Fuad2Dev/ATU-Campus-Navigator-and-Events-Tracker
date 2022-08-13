f14697b408ef1b08b489b76e961d543c369b34c9
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-08-13 03:07:57 -0500

    Revert "Update AppServiceProvider.php"
    This reverts commit 3e70bf896b6d1c568dff4f2723f4ac1c5852717d.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-use URL;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
     }
 }
