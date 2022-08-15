cf401b10b9128093321d662ee2f63d75e1e467c4
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-08-15 12:59:06 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (request()->secure()){
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
-            dd('secure connection');
         }
     }
 }
