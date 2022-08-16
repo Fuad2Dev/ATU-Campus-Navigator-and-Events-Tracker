9f913e8712d1aca75af6774e605ee1b66c5e8f9b
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-08-16 09:35:32 -0500

    updat

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,18 +16,22 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::factory()->create([
+            'id' => 1,
             'description' => 'admin'
         ]);
 
         Role::factory()->create([
+            'id' => 2,
             'description' => 'co-admin'
         ]);
 
         Role::factory()->create([
+            'id' => 3,
             'description' => 'member'
         ]);
 
         Role::factory()->create([
+            'id' => 4,
             'description' => 'pending'
         ]);
     }
