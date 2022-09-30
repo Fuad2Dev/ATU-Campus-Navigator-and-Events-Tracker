983c6cdc8aa1f020730fc76e361c6ad901024b4b
CaptainBerryWorld <94391454+Fuad2Dev@users.noreply.github.com>
2022-09-30 09:21:03 +0000

    upd: establish base file and connection
    - Mapper
    - Mapper Controller
    - Mapper Table
    - Routing (resource)
    - View
        - index
        - create
        - edit

---
+++ app/Http/Controllers/MapperController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mapper;
+use Illuminate\Http\Request;
+
+class MapperController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mapper  $mapper
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mapper $mapper)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mapper  $mapper
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mapper $mapper)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mapper  $mapper
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mapper $mapper)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mapper  $mapper
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mapper $mapper)
+    {
+        //
+    }
+}
