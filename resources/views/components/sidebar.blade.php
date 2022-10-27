381354a7a9f6975b69126331c20d1554d4ede03c
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-27 12:40:16 +0000

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="side-panel">
-    <div><a href="#!">Home</a></div>
+    <div><a href="{{route('home')}}">Home</a></div>
     <div><a href="{{route('map.2d')}}">Map</a></div>
     <div><a href="{{route('map.3d')}}">3D model</a></div>
     <div><a href="#!">Profile</a></div>
