0d1766e98a9cc6373ee9e90a38ef1f38655fd4dd
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-31 00:34:54 +0000

    Update directions.blade.php

--- resources/views/directions.blade.php
+++ resources/views/directions.blade.php
@@ -24,7 +24,6 @@
 
 <body>
     <div id='map'></div>
-    <div id="instructions"></div>
     <script>
         // add the JavaScript here
         mapboxgl.accessToken =
@@ -32,29 +31,28 @@
         const map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-0.20596, 5.55424],  // starting position
+            center: [-0.20596, 5.55424], // starting position
             zoom: 12
         });
         // set the bounds of the map
         const bounds = [
             [-0.208550, 5.550975], // Southwest coordinates
-            [-0.203386, 5.557096] // Northeast coordinates
+            [-0.203386, 5.557096]
         ];
         map.setMaxBounds(bounds);
 
         // an arbitrary start will always be the same
         // only the end or destination will change
-        const start = [-0.20596, 5.55424];
+        // const start = [-0.20596, 5.55424];
 
         // this is where the code for the next step will go
-
         // create a function to make a directions request
-        async function getRoute(end) {
-            // make a directions request using cycling profile
+        async function getRoute(start,end) {
+            // make a directions request using walking profile
             // an arbitrary start will always be the same
             // only the end or destination will change
             const query = await fetch(
-                `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`, {
+                `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`, {
                     method: 'GET'
                 }
             );
@@ -96,80 +94,60 @@
             // add turn instructions here at the end
         }
 
+        // Add geolocate control to the map.
+        // Initialize the geolocate control.
+        var geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        });
+        // Add the control to the map.
+        map.addControl(geolocate);
+
         map.on('load', () => {
+            geolocate.trigger();
+
             // make an initial directions request that
             // starts and ends at the same location
-            getRoute(start);
+            // getRoute(start);
 
             // Add starting point to the map
-            map.addLayer({
-                id: 'point',
-                type: 'circle',
-                source: {
-                    type: 'geojson',
-                    data: {
-                        type: 'FeatureCollection',
-                        features: [{
-                            type: 'Feature',
-                            properties: {},
-                            geometry: {
-                                type: 'Point',
-                                coordinates: start
-                            }
-                        }]
-                    }
-                },
-                paint: {
-                    'circle-radius': 10,
-                    'circle-color': '#3887be'
-                }
-            });
+            // map.addLayer({
+            //     id: 'point',
+            //     type: 'circle',
+            //     source: {
+            //         type: 'geojson',
+            //         data: {
+            //             type: 'FeatureCollection',
+            //             features: [{
+            //                 type: 'Feature',
+            //                 properties: {},
+            //                 geometry: {
+            //                     type: 'Point',
+            //                     coordinates: start
+            //                 }
+            //             }]
+            //         }
+            //     },
+            //     paint: {
+            //         'circle-radius': 10,
+            //         'circle-color': '#3887be'
+            //     }
+            // });
             // this is where the code from the next step will go
         });
 
-        map.on('click', (event) => {
-            const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
-            const end = {
-                type: 'FeatureCollection',
-                features: [{
-                    type: 'Feature',
-                    properties: {},
-                    geometry: {
-                        type: 'Point',
-                        coordinates: coords
-                    }
-                }]
-            };
-            if (map.getLayer('end')) {
-                map.getSource('end').setData(end);
-            } else {
-                map.addLayer({
-                    id: 'end',
-                    type: 'circle',
-                    source: {
-                        type: 'geojson',
-                        data: {
-                            type: 'FeatureCollection',
-                            features: [{
-                                type: 'Feature',
-                                properties: {},
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: coords
-                                }
-                            }]
-                        }
-                    },
-                    paint: {
-                        'circle-radius': 10,
-                        'circle-color': '#f30'
-                    }
-                });
-            }
-            getRoute(coords);
-        });
+        geolocate.on('geolocate', locateUser);
 
-        
+        function locateUser(e) {
+            console.log('A geolocate event has occurred.');
+            console.log("lng:" + e.coords.longitude + ", lat:" + e.coords.latitude)
+            var start = [e.coords.longitude, e.coords.latitude];
+            var end = [-0.2797772, 5.5687858]
+            getRoute(start, end);
+            geolocate.off('geolocate', null);
+        }
     </script>
 </body>
 
