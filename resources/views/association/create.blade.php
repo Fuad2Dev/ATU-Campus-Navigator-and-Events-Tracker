d7a50bc5058c8f8f4c219c6a5dd5cf7e9a0ab638
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-31 12:42:02 +0000

    Update create.blade.php

--- resources/views/association/create.blade.php
+++ resources/views/association/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout >
 
-    <div class="d-flex justify-content-center position-relative container">
+    {{-- <div class="d-flex justify-content-center position-relative container">
         <x-inc.association-img  size='100'/>
 
         <div class="position-absolute bottom-0 end-2 flex-column d-flex">
@@ -8,7 +8,7 @@
             {{-- <div class="m-1"></div> --}}
             <div class="btn btn-danger">Remove</div>
         </div>
-    </div>
+    </div> --}}
     <form class="container mx-auto row g-4 mt-4" method="POST" action="{{route('association.store')}}">
         @csrf
         <x-form.text-box label="name" name="name"   />
